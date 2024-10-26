5ab7b719be0dfc66c3bc933fb9bb5d9f9a7f105e
mari_iochi <2803818650@qq.com>
2024-10-26 22:29:17 +0800

    "添加了外星人，目前外星人不可移动，不可被射掉"

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from pygame.sprite import Group
+from alien import Alien
 def run_game():
     """初始化游戏并创建一个屏幕对象"""
     pygame.init()
@@ -17,21 +18,30 @@ def run_game():
 
     # 创建一个用于储存子弹的编组
     bullets=Group()
+
+    # 创建一个外星人
+    # alien = Alien(ai_settings, screen)
+
+    # 创建外星人群
+    aliens=Group()
+    gf.create_fleet(ai_settings,screen,aliens)
     # 设置背景色
     bg_color=(230,230,230)
+
+
 
     # 开始游戏的主循环
     while True:
 
         # 监视键盘和鼠标事件
-        gf.check_events(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,ship,aliens,bullets)
         ship.update()
 
         # print(len(bullets))
         # 每次循环都重绘屏幕 让最近绘制的屏幕可见
 
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 
 run_game()
